f5864318e62c40ba2ffd9145e56a8f225a962915
Marshal-Emanuel <marshcodes@gmail.com>
2023-04-03 03:24:43 +0300

    late update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Take in URL, display status code only; Usage: ./100-status_code.sh 0.0.0.0:5000/nop ; echo ""
-curl -o /dev/null -w '%{http_code}' -sLI "$1"
+# sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
